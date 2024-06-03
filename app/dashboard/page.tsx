668389ef8a3760166dac71ca344fc01b21b8e84d
tradervicky <vickygupta031@gmail.com>
2024-06-03 23:41:09 +0530

    convex configuration and add and get data added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,37 @@
 'use client'
 import { Button } from "@/components/ui/button"
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs"
+import { api } from "@/convex/_generated/api"
+import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+import { useConvex, useMutation, useQuery } from "convex/react"
+import { convexToJson } from "convex/values"
+import { useEffect } from "react"
 
 const Dashboard = () => {
+  const {user}:any = useKindeBrowserClient();
+  const convex = useConvex()
+  // const getUser = useQuery(api.user.getUser, {email: user?.email})
+  useEffect(() => {
+   if(user){
+   checkUser()
+   }
+  }, [user])
+
+  const checkUser = async()=>{
+    const result = await convex.query(api.user.getUser, {email:user?.email})
+    if(!result?.length){
+      createUser({
+        name:user.given_name,
+        email: user.email,
+        image : user.picture
+      })
+      .then((resp)=>{
+        console.log(resp)
+      })
+    }
+  }
+  
+const createUser = useMutation(api.user.createUser)
+
   return (
     <div>
         Dashboard
